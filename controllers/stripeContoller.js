830263953e5b54c5ae71d8c942265a131164221f
Ashwani <aswintalk76@gmail.com>
2023-08-27 15:57:55 +0530

    stripe

--- controllers/stripeContoller.js
+++ controllers/stripeContoller.js
@@ -4,3 +4,20 @@ exports.testStripeController = async (req, res) => {
     message: "test Stripe Controller working",
   });
 };
+
+//Workig on plans module start
+exports.createPlanStripeController = async (req, res) => {
+  return res.status(201).send({
+    success: true,
+    message: "create-plan Stripe Controller working",
+  });
+};
+
+exports.updatePlanStripeController = async (req, res) => {
+  return res.status(201).send({
+    success: true,
+    message: "create-plan Stripe Controller working",
+  });
+};
+
+//Workig on plans module End
--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { testStripeController } = require("../controllers/stripeContoller");
+const { testStripeController, createPlanStripeController } = require("../controllers/stripeContoller");
 //router object
 const router = express.Router();
 
 //Start a code
 
 router.get("/test", testStripeController);
+//Workig on plans module start
+router.get("/create-plan", createPlanStripeController); //post
+router.get("/update-plan", createPlanStripeController); //put /update-plan/:planId
+
+
+
+//Workig on plans module End
 
 //End a code
 
