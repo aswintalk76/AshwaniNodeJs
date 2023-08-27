9d7a69323684d7fb99e3e68511d994e22436f3e4
Ashwani <aswintalk76@gmail.com>
2023-08-27 08:01:33 +0530

    create a new server working

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello I am working");
+const express = require("express");
+const colors = require("colors");
+
+//rest objecct
+const app = express();
+const port = process.env.port || 5000;
+const DEV_MODE = process.env.port || "Development";
+
+//create a default route
+app.get("/", (req, res) => {
+  res.send(`Server is working on ${port}`);
+});
+
+//listenI
+app.listen(port, () => {
+  console.log(
+    `Server Running on ${DEV_MODE} mode port no ${port}`.bgCyan.white
+  );
+});
